89587f37d9bc6d7b91b28ef1de805e9f4c5b6c6f
skeptycal <skeptycal@gmail.com>
2020-06-26 16:58:29 -0500

    clear all data

---
+++ __init__.py
@@ -0,0 +1,19 @@
+""" autobin
+    ~/bin directory test package
+    """
+
+# ~/bin directory package
+__version__ = "0.0.1"
+__author__ = "Michael Treanor (skeptycal@gmail.com)"
+__copyright__ = "(c) 2020 Michael Treanor. MIT License"
+
+import requests
+
+# print(dir())
+
+# for k in dir():
+#     try:
+#         v = repr(eval (f'{k}'))
+#     except:
+#         v = '...'
+#     print(f"{k:<15.15}: {v:35.35}")
