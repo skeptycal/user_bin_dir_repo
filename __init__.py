720570cf0d38d343dd28649e12d0244944bfdb6c
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-07-05 04:58:33 -0500

    # (GitIt) minor updates # https://www.github.com/skeptycal

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ __version__ = "0.0.1"
 __author__ = "Michael Treanor (skeptycal@gmail.com)"
 __copyright__ = "(c) 2020 Michael Treanor. MIT License"
 
-import requests
+# import requests
 
 # print(dir())
 
--- bc_remove.sh
+++ bc_remove.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env zsh
 # -*- coding: utf-8 -*-
-# shellcheck shell=bash
-# shellcheck source=/dev/null
-# shellcheck disable=2230
+    # shellcheck shell=bash
+    # shellcheck source=/dev/null
+    # shellcheck disable=2230
 
-# cannot be run directly; called from .bash_profile or .bashrc
 #?#- ##########################-
-#* .functions - functions for standard script modules (ssm)
+#* bc_remove.sh - remove python bytecode cache files and directories
 #* copyright (c) 2019 Michael Treanor
 #* MIT License - https://www.github.com/skeptycal
 
 # set -a
 # set -aET # ET - inherit traps
-export SET_DEBUG=0 # set to 1 for verbose testing
+
+# inherit SET_DEBUG; default 0; set to 1 for verbose testing
+SET_DEBUG=${SET_DEBUG:-0}
 
 exec 6>&1 # non-volatile stdout leaves return values of stdout undisturbed
 # return 0
