235dc5479eaf31c056165c13205f9147506c4b39
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-08-26 00:59:57 -0500

    revisions

--- tests/test_html2txt.py
+++ tests/test_html2txt.py
@@ -5,8 +5,8 @@ from pathlib import Path
 from sys import path as PYTHON_PATH
 
 
-PYTHON_PATH.insert(-1,Path(__file__).resolve().parents[0].as_posix())
-PYTHON_PATH.insert(-1,Path(__file__).resolve().parents[1].as_posix())
+PYTHON_PATH.insert(-1, Path(__file__).resolve().parents[0].as_posix())
+PYTHON_PATH.insert(-1, Path(__file__).resolve().parents[1].as_posix())
 
 PYTHON_PATH = list(set(PYTHON_PATH))
 
@@ -14,15 +14,10 @@ PYTHON_PATH = list(set(PYTHON_PATH))
 # import html2txt
 # from html2txt import onlywhite2, onlywhite
 
+
 class TestOnlyWhite:
     def test_only_white_vs_isspace(self):
-        lines=[
-            ' df sdwlkjlkd9788723',
-            '    ',
-            '\n',
-            ' sdfkkj     ',
-            '\t\t   '
-            ]
+        lines = [" df sdwlkjlkd9788723", "    ", "\n", " sdfkkj     ", "\t\t   "]
 
         for c in lines:
             # for c in line:
@@ -31,7 +26,8 @@ class TestOnlyWhite:
             # print(onlywhite(c))
             # print(onlywhite2(c))
             # assert c.isspace() == onlywhite(c)
-                # assert (onlywhite(c) == c in whitespace)
-                # assert (onlywhite(c) == c.isspace())
+            # assert (onlywhite(c) == c in whitespace)
+            # assert (onlywhite(c) == c.isspace())
+
 
 print(repr(whitespace))
